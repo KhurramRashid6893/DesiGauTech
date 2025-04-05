f3e957c3f97047a8988fa03b3338d7f5ba3d50aa
Khurram Rashid <98824765+KhurramRashid6893@users.noreply.github.com>
2025-04-05 21:51:18 +0530

    app.py

--- app.py
+++ app.py
@@ -53,6 +53,6 @@ def contact():
     return render_template("contact.html")
 
 if __name__ == "__main__":
-    app.run(debug=True
+    app.run(debug=True)
 # if 'render' in os.environ.get('SERVER_SOFTWARE', '').lower():
 #     app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
